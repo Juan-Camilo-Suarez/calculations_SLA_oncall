3ee4096e306c2247e054599d0d8d7f88f43fa4f6
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2023-11-29 11:30:56 +0300

    + refactor config

---
+++ configs.py
@@ -0,0 +1,20 @@
+import logging
+
+from environs import Env
+
+env = Env()
+env.read_env()
+
+
+class Config(object):
+    prometheus_api_url = env("PROMETHEUS_API_URL", 'http://localhost:9090')
+    scrape_interval = env.int("SCRAPE_INTERVAL", 60)
+    log_level = env.log_level("LOG_LEVEL", logging.INFO)
+
+    mysql_host = env("MYSQL_HOST", 'localhost')
+    mysql_port = env.int("MYSQL_PORT", '3306')
+
+    mysql_user = env("MYSQL_USER", 'root')
+    mysql_password = env("MYSQL_PASS", '1234')
+
+    mysql_db_name = env("MYSQL_DB_NAME", 'sla')

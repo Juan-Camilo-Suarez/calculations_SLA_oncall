4f8f709c5569ae7283d1c7812dd45df69c4980cd
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2023-11-29 16:20:56 +0300

    + added migrations and save operation

--- db.py
+++ db.py
@@ -23,3 +23,30 @@ class Mysql:
         logging.info("creating db")
         cursor.execute(f"CREATE DATABASE IF NOT EXISTS {config.mysql_db_name}")
         cursor.execute('USE sla')
+
+        cursor = self.connection.cursor()
+
+        cursor.execute("""
+                CREATE TABLE IF NOT EXISTS %s (
+                    datetime datetime not null default NOW(),
+                    name varchar(255) not null,
+                    slo float(4) not null,
+                    value float (4) not null,
+                    is_bad bool not null default false
+                )
+            """ % (self.table_name))
+
+        cursor.execute("""
+                ALTER TABLE %s ADD INDEX (datetime)
+            """ % (self.table_name))
+
+        cursor.execute("""
+                ALTER TABLE %s ADD INDEX (name)
+            """ % (self.table_name))
+
+    def save_indicator(self, name, slo, value, is_bad=False, time=None):
+        cursor = self.connection.cursor()
+        sql = f"INSERT INTO {self.table_name} (name, slo, value, is_bad, datetime) VALUES (%s, %s, %s, %s, %s)"
+        val = (name, slo, value, int(is_bad), time)
+        cursor.execute(sql, val)
+        self.connection.commit()

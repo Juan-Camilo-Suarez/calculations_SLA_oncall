4ce02078fd5303d65ca2fa7c1e21307123c94a65
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2023-11-29 14:48:25 +0300

    + added creation database

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 import logging
 
-import mysql
+import mysql.connector
 
 from configs import Config
 
@@ -8,10 +8,18 @@ from configs import Config
 class Mysql:
     def __init__(self, config: Config) -> None:
         logging.info("connecting to Mysql")
+
         self.connection = mysql.connector.connect(
             host=config.mysql_host,
+            port=config.mysql_port,
             user=config.mysql_user,
             passwd=config.mysql_password,
             auth_plugin='mysql_native_password'
         )
         self.table_name = 'indicators'
+
+        cursor = self.connection.cursor()
+
+        logging.info("creating db")
+        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {config.mysql_db_name}")
+        cursor.execute('USE sla')
